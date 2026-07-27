8b3fc868642cf2ad4e5fea5f6369854dd633b4f2
lrndwy <hafiztamvan15@gmail.com>
2026-07-28 01:47:45 +0700

    fix(migrations): tambahkan -- +migrate Up directive pada dua file migrasi

--- backend/migrations/20260727000000_template_category_nullable.sql
+++ backend/migrations/20260727000000_template_category_nullable.sql
@@ -1,3 +1,2 @@
--- Pisahkan kategori dari template surat:
--- category_id pada letter_templates sekarang nullable.
+-- +migrate Up
 ALTER TABLE letter_templates ALTER COLUMN category_id DROP NOT NULL;
--- backend/migrations/20260728000000_make_letter_template_category_nullable_again.sql
+++ backend/migrations/20260728000000_make_letter_template_category_nullable_again.sql
@@ -1,3 +1,2 @@
--- Perbaiki isu: kolom category_id di letter_templates masih NOT NULL.
--- Pastikan kolom category_id di tabel letter_templates menjadi nullable.
+-- +migrate Up
 ALTER TABLE letter_templates ALTER COLUMN category_id DROP NOT NULL;
